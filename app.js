f4560f72c18ca9ee44640cf93b35c3be09db1c35
ramkrishs <ramkrishs@outlook.com>
2019-05-04 19:58:08 -0700

    remove elasticbeanstack

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express");
  
 var app = express();
  
+app.use(express.static('public'));
  
 //make way for some custom css, js and images
 app.use('/css', express.static(__dirname + '/public/css'));
